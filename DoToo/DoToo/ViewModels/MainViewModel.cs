da09ae2f4f8ba7d9077786ab68ee53e7c671e122
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-03-28 19:33:17 -0300

    Primeiro commit

---
+++ DoToo/DoToo/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DoToo.Repositories;
+using DoToo.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DoToo.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        private readonly TodoItemRepository repository;
+
+        public MainViewModel(TodoItemRepository repository)
+        {
+            this.repository = repository;
+            Task.Run( async () => await LoadData() );
+        }
+
+        private async Task LoadData()
+        { 
+
+        }
+
+        // Evento de clique no botão para adicionar item
+        public ICommand AddItem => new Command( async () => {
+
+            var itemView = Resolver.Resolve<ItemView>();
+            await Navigation.PushAsync( itemView );
+
+        } );
+    }
+}
---
+++ DoToo/DoToo/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DoToo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoToo.ViewModels
+{
+    public class TodoItemViewModel : ViewModel
+    {
+        public TodoItem Item { get; private set; }
+
+        public TodoItemViewModel(TodoItem item) => Item = item;
+
+        public event EventHandler ItemStatusChanged;
+
+        public string StatusText => 
+            (Item.Completed ? "Reativar" : "Completo");
+    
+        /* A instrução acima é a mesma coisa que esta
+         * Só que está com notação funcional => (LAMBDA)
+        public string StatusText()
+        {
+            if (Item.Completed)
+                return "Reativar";
+            else
+                return "Completo";
+        }
+        */
+
+    }
+}
