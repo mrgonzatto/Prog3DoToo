987497af4973e51e88ab36dcbe1c3cff5f575d09
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-03-28 20:54:19 -0300

    Aula 28/03/23

--- DoToo/DoToo/ViewModels/MainViewModel.cs
+++ DoToo/DoToo/ViewModels/MainViewModel.cs
@@ -1,8 +1,9 @@
-﻿using DoToo.Repositories;
+﻿using DoToo.Models;
+using DoToo.Repositories;
 using DoToo.Views;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -13,15 +14,36 @@ namespace DoToo.ViewModels
     {
         private readonly TodoItemRepository repository;
 
+        public ObservableCollection<TodoItemViewModel> Items { get; set; }
+
         public MainViewModel(TodoItemRepository repository)
         {
+            repository.OnItemAdded += (sender, item) =>
+                Items.Add( CreateTodoItemViewModel(item) );
+
+            repository.OnItemUpdated += (sender, item) =>
+                Task.Run(async () => await LoadData());
+
             this.repository = repository;
             Task.Run( async () => await LoadData() );
         }
 
         private async Task LoadData()
-        { 
+        {
+            var items = await repository.GetItems();
+            var itemViewModels = items.Select( i => CreateTodoItemViewModel(i) );
+            Items = new ObservableCollection<TodoItemViewModel>(itemViewModels);
+        }
 
+        private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+        { 
+            var itemViewModel = new TodoItemViewModel(item);
+            itemViewModel.ItemStatusChanged += ItemStatusChanged;
+            return itemViewModel;
+        }
+
+        private void ItemStatusChanged(object sender, EventArgs e)
+        { 
         }
 
         // Evento de clique no botão para adicionar item
